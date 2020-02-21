c3e786321d242b95a14f4ce3a84828220eedbc5b
Salvatore <salva.95f@gmail.com>
2020-02-21 15:41:04 +0100

    updates

--- db/migrate/20200220115004_create_users.rb
+++ db/migrate/20200220115004_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :email
       t.string :password_digest
+      t.string :address
+      t.string :mobile
 
       t.timestamps
     end
