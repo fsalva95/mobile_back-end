18ba495c2db7c1238434b4c5cba081ec93552ca4
Salvatore <salva.95f@gmail.com>
2020-02-23 19:55:47 +0100

    updates

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,12 +2,13 @@ class JsonWebToken
  class << self
    def encode(payload, exp = 24.hours.from_now)
      payload[:exp] = exp.to_i
-     JWT.encode(payload, ENV['SECRET_KEY_BASE'])
+     JWT.encode(payload, ENV['SECRET_KEY_BASE'])   #Rails.application.secrets.secret_key_base in localhost
+                                                                      #ENV['SECRET_KEY_BASE'] in heroku
    end
 
    def decode(token)
-     body = JWT.decode(token, ENV['SECRET_KEY_BASE'])[0]
-     HashWithIndifferentAccess.new body
+     body = JWT.decode(token, ENV['SECRET_KEY_BASE'])[0] #Rails.application.secrets.secret_key_base
+     HashWithIndifferentAccess.new body                                      #ENV['SECRET_KEY_BASE'] in heroku
    rescue
      nil
    end
