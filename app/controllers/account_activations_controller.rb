91d6730544bd85718fb22cd8a49c99448f2cd23a
fsalva95 <52610638+fsalva95@users.noreply.github.com>
2020-05-01 19:31:49 +0200

    Update account_activations_controller.rb

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -8,9 +8,7 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
-      #log_in gamer
-      #flash[:success] = "Account activated!"
-      #redirect_to new_account_attribute_url(email: gamer.email)     #memorizzo i dati aggiuntivi richiesti per il gamer
+   
     else
       #flash[:danger] = "Invalid activation link"
       #redirect_to root_url
